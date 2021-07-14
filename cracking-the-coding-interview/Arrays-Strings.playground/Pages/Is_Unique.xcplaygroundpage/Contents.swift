2750326f5a907b6328caeaeb74b73816e6554646
Jocelyn B <jocelynboyd@pursuit.org>
2021-07-14 18:01:31 -0400

    clean up arrays and string playground

--- cracking-the-coding-interview/Arrays-Strings.playground/Pages/Is_Unique.xcplaygroundpage/Contents.swift
+++ cracking-the-coding-interview/Arrays-Strings.playground/Pages/Is_Unique.xcplaygroundpage/Contents.swift
@@ -2,14 +2,9 @@
 import Cocoa
 
 /*:
-Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+Implement an algorithm to determine if a string has all unique characters.
+What if you cannot use additional data structures?
 */
-
-// generate sample strings
-func randomString(length: Int) -> String {
-  let letters = "abcdefghijklmnopqrstuvwxyz"
-  return String((0..<length).map{ _ in letters.randomElement()! })
-}
 
 // input: string
 // output: bool
@@ -18,35 +13,37 @@ func randomString(length: Int) -> String {
 // constraints: can't use additional data structures
 // edge cases: no input, string has one character
 
-// check the length of the string, if string length is < 2, return true
-// instatiate a dictionary - key: character, value: count
-// dictionary lookup - O(1)
-// loop through each character in the string O(n)
-// check if character is in dictionary
-// if yes, return false
-// if no, put it in the dictionary with the value of 1
-// return true
-
 func isUnique(input: String) -> Bool {
     
-    if input.count < 2 {
-        return true
-    }
+    //check the length of the string, return if less than 2
+    if input.count < 2 { return true }
     
+    //instatiate a dictionary - key: character, value: count
     var stringDict = [Character:Int]()
     
+    //loop through each character in the string - O(n)
     for element in input {
-        print(element)
+        //if the the character is in the dictionary, return false
         if let _ = stringDict[element] {
             return false
         } else {
+            //else, put it in the dictionary with a value of 1
             stringDict[element] = 1
         }
     }
+    //If we got this far in our code without returning false,
+    //then it means that all characters in the string are unique
     return true
 }
 
-var input = "lgvsvxggxzzkknjqtjqtcqgva" //randomString(length: 25)
-print(input)
-print(isUnique(input: input))
+
+//function to generate sample input strings
+func randomString(length: Int) -> String {
+  let letters = "abcdefghijklmnopqrstuvwxyz"
+  return String((0..<length).map{ _ in letters.randomElement()! })
+}
+
+var input = randomString(length: 25)
+var result = isUnique(input: input)
+print("\(input) -> \(result)")
 //: [Next](@next)
