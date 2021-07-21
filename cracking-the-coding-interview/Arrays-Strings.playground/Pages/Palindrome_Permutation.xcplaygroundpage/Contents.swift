c792c7b27fecf6fcde91f382dc9eb8a5c465a708
Agarrovi1 <52180618+Agarrovi1@users.noreply.github.com>
2021-07-21 19:06:49 -0400

    Adds palindrome permutation solutions

--- cracking-the-coding-interview/Arrays-Strings.playground/Pages/Palindrome_Permutation.xcplaygroundpage/Contents.swift
+++ cracking-the-coding-interview/Arrays-Strings.playground/Pages/Palindrome_Permutation.xcplaygroundpage/Contents.swift
@@ -10,4 +10,50 @@ Output: True (permutations: 'taco cat', 'atco cta', etc.)
 Hints: #106, #121, #134, #136
 */
 
+func isPalindromePermutation(str: String) -> Bool {
+    let cleanStr = str.lowercased().replacingOccurrences(of: " ", with: "")
+    var freqDict = [Character:Int]()
+    
+    for char in cleanStr {
+        if let value = freqDict[char] {
+            freqDict[char] = value + 1
+        } else {
+            freqDict[char] = 1
+        }
+    }
+    
+    var oddFreqCount = 0
+    
+    for value in freqDict.values {
+        if value % 2 != 0 {
+            oddFreqCount += 1
+        }
+    }
+    
+    return oddFreqCount <= 1
+}
+
+// reer, r:2, e:2
+//civic c:2, i:2, v:1
+
+isPalindromePermutation(str: "civic")
+isPalindromePermutation(str: "Tact Cao")
+
+//interviewCake solution O(n)
+func hasPalindromePermutation(str: String) -> Bool {
+    //To track characters we've seen an odd number of times
+    var unpairedCharacters = Set<Character>()
+    
+    for char in str {
+        if unpairedCharacters.contains(char) {
+            unpairedCharacters.remove(char)
+        } else {
+            unpairedCharacters.insert(char)
+        }
+    }
+    
+    //The string has a palindrome permutation if it has one or zero character without a pair
+    return unpairedCharacters.count <= 1
+}
+
 //: [Next](@next)
