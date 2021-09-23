0924ec8f1974bbce63aba1fc93889d00886440a4
Jocelyn B <jocelynboyd@pursuit.org>
2021-09-23 19:25:26 -0400

    delete cracking the coding interview file

--- cracking-the-coding-interview/Arrays-Strings.playground/Pages/Zero_Matrix.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: [Previous](@previous)
-import Foundation
-
-/*:
-Write an algorithm such that if an element in an M * N matrix is 0, its entire row and column are set to 0.
-Hints: #17, #74, #102
-*/
-
-//: [Next](@next)
